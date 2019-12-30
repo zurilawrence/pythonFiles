d34afa042150d71127e151b22b47618b202316c0
zurilawrence <zuriehl@gmail.com>
2019-12-30 16:58:04 +0000

    Update filling

--- formatSonetoPayments.py
+++ formatSonetoPayments.py
@@ -4,19 +4,22 @@ import sys
 import pandas as pd
 
 def formatPaymentsExport(dfile):
-    dfile.drop("textbox2","textbox48","textbox20","textbox50","textbox13","textbox18","textbox17","textbox8","textbox22","textbox5")
+    dfile.drop("textbox2","textbox48","textbox20","textbox50","textbox13","textbox18","textbox17","textbox8","textbox22","textbox15")
 
-    #Add new header
-    dfile.columns = ['Client','PostingDate','AmountPaid','PaymentNo','Invoice','ServiceDate')
+    # Add new header
+    dfile.columns = ['Client','PostingDate','AmountPaid','PaymentNo','Invoice','ServiceDate','Service')
 
-    #Remove first row (unneeded text)
+    # Remove first row (unneeded text)
     dfile.drop(['0'], axis='0')
 
-    #Format dates
+    # Format dates
     dfile = pd.to_datetime(df.PostingDate)
     dfile = pd.to_datetime(df.ServiceDate)
     dfile['PostingDate'] = dfile['PostingDate'].dfile.strftime('%m/%d/%Y')
     dfile['ServiceDate'] = dfile['ServiceDate'].dfile.strftime('%m/%d/%Y')
+
+    # Change payment values to positive
+    dfile.loc['AmountPaid'] *= -1
 
     return dfile
 
--- verifyPayments.py
+++ verifyPayments.py
@@ -3,5 +3,5 @@ import sys
 import Flask 
 
 def authorizePayments():
-    
+
 
