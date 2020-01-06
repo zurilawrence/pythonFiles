abaaccb25cac49f330d955647bdf9ec5d76a7207
zurilawrence <zuriehl@gmail.com>
2020-01-06 16:17:13 +0000

    Global method

--- authcheck.py
+++ authcheck.py
@@ -5,28 +5,31 @@ from flask_mail import Mail
 from flask_mail import Message
 from datetime import datetime
 
+def emailAlert(dfile):
+    pass
+
+def expirationCheck(dfile):
+    # Format date
+    dfile = pd.to_datetime(df.Cert End Date)
+    dfile['Cert_End_Date'] = dfile['Cert_End_Date'].dfile.strftime('%Y,%m,%d')
+
+    # Compare authorization to expiration date
+    today = datetime.now()
+    dfile[((dfile['Cert_End_Date'] - today).days <= 14)]
+
+    # Call email fnc on expiring auth
+    emailAlert(dfile)
+    print(df)
+
 class Update:
     def __init__(self, fname, lname, code, auth):
         self.fname = fname
         self.lname = lname
         self.code = code
         self.auth = auth
-     
-    def expirationCheck(dfile):
-        # Format date
-        dfile = pd.to_datetime(df.Cert_End_Date)
-        dfile['Cert_End_Date'] = dfile['Cert_End_Date'].dfile.strftime('%Y,%m,%d')
-
-        # Compare authorization to expiration date
-        today = datetime.now()
-        dfile[((dfile['Cert_End_Date'] - today).days <= 14)]
-
-        # Call email fnc on expiring auth
-        # emailAlert(dfile)
-        print(df)
 
     def operation():
         df = pd.read_excel(sys.argv[1],index_col=0)
         expirationCheck(df)
-
+        
     operation()
