22534556bef39cbb3eb9362a62d053480cbdc8cb
zurilawrence <zuriehl@gmail.com>
2020-01-14 17:32:22 +0000

    Debug: AmountPaid edit

--- formatSonetoPayments.py
+++ formatSonetoPayments.py
@@ -3,26 +3,20 @@
 import sys
 import pandas as pd
 
-def formatPaymentsExport(dfile):
-    dfile.drop("textbox2","textbox48","textbox20","textbox50","textbox13","textbox18","textbox17","textbox8","textbox22","textbox15")
-
-    # Add new header
-    dfile.columns = ['Client','PostingDate','AmountPaid','PaymentNo','Invoice','ServiceDate','Service')
-
-    # Remove first row (unneeded text)
-    dfile.drop(['0'], axis='0')
-
+def formatPayments(dfile):
+    dfile.rename(columns={"textbox31":"Client","textbox57":"PostingDate","textbox6":"AmountPaid","textbox7":"PaymentNo","textbox32":"Invoice","textbox43":"ServiceDate"},inplace=True)
+ 
     # Format dates
-    dfile = pd.to_datetime(df.PostingDate)
-    dfile = pd.to_datetime(df.ServiceDate)
-    dfile['PostingDate'] = dfile['PostingDate'].dfile.strftime('%m/%d/%Y')
-    dfile['ServiceDate'] = dfile['ServiceDate'].dfile.strftime('%m/%d/%Y')
-
+    dfile['PostingDate'] = pd.to_datetime(dfile.PostingDate)
+    dfile['ServiceDate'] = pd.to_datetime(dfile.ServiceDate)
+    dfile['PostingDate'] = dfile['PostingDate'].dt.strftime('%m/%d/%Y')
+    dfile['ServiceDate'] = dfile['ServiceDate'].dt.strftime('%m/%d/%Y')
+    
     # Change payment values to positive
-    dfile.loc['AmountPaid'] *= -1
+    for idx in range(len(dfile.index)):
+        dfile.loc[idx, 'AmountPaid'] = float(dfile.loc[idx, 'AmountPaid'])
 
     return dfile
 
-dfile = pd.read_tocsv(sys.argv[1])
-df = formatPaymentsExports(dfile)
-return df
+dfile = pd.read_csv(sys.argv[1])
+formatPayments(dfile)
